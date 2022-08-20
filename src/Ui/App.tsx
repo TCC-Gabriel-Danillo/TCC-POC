3038a3668886c02c93576a32a669f5def11dc049
GabrielDvPereira <gdavi.gd@gmail.com>
2022-08-19 21:33:10 -0300

    chore: config firebase

--- src/Ui/App.tsx
+++ src/Ui/App.tsx
@@ -1,6 +1,7 @@
+import 'dotenv/config'
+import './src/config/firebaseConfig'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useEffect } from "react"
 
 export default function App() {
   return (
