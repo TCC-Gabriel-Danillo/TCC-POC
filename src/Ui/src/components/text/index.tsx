51fc0ba662b226611e2ab5228027c277486677b3
GabrielDvPereira <gdavi.gd@gmail.com>
2022-08-21 20:14:54 -0300

    feat: create input component

--- src/Ui/src/components/text/index.tsx
+++ src/Ui/src/components/text/index.tsx
@@ -1,24 +1,24 @@
 import { ReactNode } from 'react';
 import { TextStyle, Text as RnText } from 'react-native';
-import { textStyles } from "./styles"
+import { styles } from "./styles"
 
 interface Props {
     fontType?: 'h1' | 'h2' | 'p'
     fontWeight?: 'bold' | 'semibold' | 'regular'
-    styles?: TextStyle
+    style?: TextStyle
     children: ReactNode
 }
 
 export const Text: React.FC<Props> = ({
     fontType = 'p', 
     fontWeight = 'regular',
-    styles,
+    style,
     children
  }) => {
-  const fontTypeStyle = textStyles[fontType]; 
-  const fontWeightStyle = textStyles[fontWeight];
+  const fontTypeStyle = styles[fontType]; 
+  const fontWeightStyle = styles[fontWeight];
   return(
-    <RnText style={[fontTypeStyle, fontWeightStyle, styles]}>
+    <RnText style={[fontTypeStyle, fontWeightStyle, style]}>
         {children}
     </RnText>
   )
