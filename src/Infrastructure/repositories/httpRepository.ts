d56e0341d11ee999105a54faf43b5b564ba22ba1
GabrielDvPereira <gdavi.gd@gmail.com>
2022-08-21 17:47:19 -0300

    feat: implement http repository

--- src/Infrastructure/repositories/httpRepository.ts
+++ src/Infrastructure/repositories/httpRepository.ts
@@ -1,9 +1,34 @@
-import { HttpRepository } from "../../Domain/repositories";
 
-class HttpRepositoryImp implements HttpRepository {
-    get: <T>(path: string, params?: Record<string, any> | undefined, config?: any) => Promise<any>;
-    post: <T>(path: string, params?: Record<string, any> | undefined, config?: any) => Promise<any>;
-    put: <T>(path: string, params?: Record<string, any> | undefined, config?: any) => Promise<any>;
-    delete: <T>(path: string, params?: any, config?: any) => Promise<any>;
-    
+import { HttpRepository } from "../../Domain/repositories";
+import axios, { AxiosInstance } from "axios"; 
+
+
+const headers = {
+    'Content-Type': 'application/json'
+};
+
+export class HttpRepositoryImp implements HttpRepository {
+    api: AxiosInstance;
+    constructor(baseURL: string){
+        this.api = axios.create({
+            baseURL, 
+            headers
+        })
+    }
+    async get <T>(endpoint: string, params?: Record<string, any>, config?: any) {
+        const response = await this.api.get(endpoint, { ...config, params });
+        return response.data as T;
+    }
+    async post <T>(endpoint: string, params?: Record<string, any>, config?: any) {
+        const response = await this.api.post(endpoint, { ...params }, { ...config });
+        return response.data as T;
+    }
+   async put <T>(endpoint: string, params?: Record<string, any>, config?: any) {
+        const response = await this.api.put(endpoint, { ...params }, { ...config });
+        return response.data as T;
+    }
+    async delete <T>(endpoint: string, params?: any, config?: any) {
+        const response = await this.api.delete(endpoint, { ...config, params });
+        return response.data as T;
+    }
 }
